65aeda770dbbb4360e6d269456cfa62be1964376
sjoerdsommen <sjoerdsommen@gmail.com>
2021-10-11 16:48:22 +0200

    check if fork was successful

--- tests/test_actions.py
+++ tests/test_actions.py
@@ -1,5 +1,5 @@
 from brownie import accounts
 
 def test_github_actions():
-    assert accounts[0].balance() > 0
-    assert accounts[0].address == '0x8486e1EC28Ca6cb008AFAf648675E599aA687110'
+    assert accounts[0].balance() > 1000000000000000000000
+
