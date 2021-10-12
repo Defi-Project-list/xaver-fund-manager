f07884f70b08d95b426e679291261c9f8ff6d6e3
sjoerdsommen <sjoerdsommen@gmail.com>
2021-10-12 11:19:20 +0200

    Made DAI interface by locally copying the ABI

--- tests/test_github_actions.py
+++ tests/test_github_actions.py
@@ -1,15 +1,9 @@
-from brownie import accounts, network
-from brownie_tokens import MintableForkToken
+from brownie import accounts, interface
 
-# def test_balance_forked_account():
-#     if network.show_active() == 'development': # on github
-#         assert accounts[10].balance() > 1000000000000000000000 
-#     assert accounts[0].balance() > 0 # also locally
-
-def test_mintable_fork_token_dai():
+def test_balance_forked_account():
     _dai = "0x6B175474E89094C44Da98b954EedeAC495271d0F"
+    dai = interface.dai(_dai)
 
-    dai = MintableForkToken(_dai)
-    dai._mint_for_testing(accounts[0], 10000)
-
-    assert dai.balanceOf(accounts[0]) == 10000
+    assert accounts[10].address == "0x21a31Ee1afC51d94C2eFcCAa2092aD1028285549"
+    assert accounts[10].balance() > 1000000000000000000000 
+    assert dai.balanceOf(accounts[10]) > 100
