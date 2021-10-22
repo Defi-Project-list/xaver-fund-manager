79b839b61d8c190e703a56fb42f7fc86d3452417
Theezr <nickvanrossenberg@hotmail.com>
2021-10-22 13:41:46 +0200

    hardhat init

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,7 +3,7 @@
 //
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
-const hre = require("hardhat");
+import { ethers } from "hardhat";
 
 async function main() {
   // Hardhat always runs the compile task when running scripts with its command
@@ -14,8 +14,8 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const CompoundSupply = await hre.ethers.getContractFactory("CompoundSupply");
-  const compoundSupply = await CompoundSupply.deploy();
+  const Greeter = await ethers.getContractFactory("Greeter");
+  const greeter = await Greeter.deploy("Hello, Hardhat!");
 
   await greeter.deployed();
 
--- tests/test_github_actions.py
+++
@@ -1,9 +0,0 @@
-from brownie import accounts, interface
-
-def test_balance_forked_account():
-    _dai = "0x6B175474E89094C44Da98b954EedeAC495271d0F"
-    dai = interface.dai(_dai)
-
-    assert accounts[10].address == "0x21a31Ee1afC51d94C2eFcCAa2092aD1028285549"
-    assert accounts[10].balance() > 1000000000000000000000 
-    assert dai.balanceOf(accounts[10]) > 100
